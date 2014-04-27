7efddcc755c84ecb6a768bfb43ac7c5b3f0f6c63
MeredithU <me@meredithunderell.com>
2014-04-27 12:26:02 -0700

    Moved filter function from controllers.js to filters.js

--- app/js/filters.js
+++ app/js/filters.js
@@ -1,10 +1,21 @@
 'use strict';
 
-/* Filters */
+yogaClassApp.filter('myFilter', function() {
+	return function(items, studios) {
+		var studioMatches = [];
 
-angular.module('myApp.filters', []).
-  filter('interpolate', ['version', function(version) {
-    return function(text) {
-      return String(text).replace(/\%VERSION\%/mg, version);
-    };
-  }]);
+		angular.forEach(items, function(item) {
+			if(studios.capitolHill == false && studios.wedgewood == false) {
+				studioMatches.push(item);
+			}
+			else if(studios.capitolHill == true && studios.wedgewood == false && item.studio == 'Capitol Hill') {
+				studioMatches.push(item);
+			}
+			else if(studios.capitolHill == false && studios.wedgewood == true && item.studio == 'Wedgewood') {
+				studioMatches.push(item);
+			}
+		});
+
+		return studioMatches;
+	};
+});
