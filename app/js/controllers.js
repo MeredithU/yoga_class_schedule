3dd22062a152386a61b7aa51d2a5331e8441aae2
MeredithU <me@meredithunderell.com>
2014-04-27 11:49:55 -0700

    Can filter by studio location using checkboxes

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -69,4 +69,26 @@ yogaClassApp.controller('YogaClassCtrl', function ($scope) {
 		'level': 'All'
 	},
 	];
+
+	$scope.studios = {capitolHill: false, wedgewood: false};
+});
+
+yogaClassApp.filter('myFilter', function() {
+	return function(items, studios) {
+		var studioMatches = [];
+
+		angular.forEach(items, function(item) {
+			if(studios.capitolHill == false && studios.wedgewood == false) {
+				studioMatches.push(item);
+			}
+			else if(studios.capitolHill == true && studios.wedgewood == false && item.studio == 'Capitol Hill') {
+				studioMatches.push(item);
+			}
+			else if(studios.capitolHill == false && studios.wedgewood == true && item.studio == 'Wedgewood') {
+				studioMatches.push(item);
+			}
+		});
+
+		return studioMatches;
+	};
 });
