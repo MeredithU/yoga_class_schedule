9720a09478a8e6f811c7ff2ec981373da976399a
MeredithU <me@meredithunderell.com>
2014-05-02 18:56:43 -0700

    Added multiple views through routing

--- app/js/app.js
+++ app/js/app.js
@@ -1,7 +1,24 @@
 'use strict';
 
 var yogaClassApp = angular.module('yogaClassApp', [
+	'ngRoute',
 	'yogaClassControllers',
 	'yogaClassFilters'
 ]);
+
+yogaClassApp.config(['$routeProvider',
+	function($routeProvider) {
+		$routeProvider.
+			when('/classes', {
+				templateUrl: 'partials/class-list.html',
+				controller: 'YogaClassCtrl'
+			}).
+			when('/classes/:classID', {
+				templateUrl: 'partials/class-detail.html',
+				controller: 'YogaClassDetailCtrl'
+			}).
+			otherwise({
+				redirectTo: '/classes'
+			});
+	}]);
 
--- app/js/controllers.js
+++ app/js/controllers.js
@@ -2,7 +2,8 @@
 
 var yogaClassControllers = angular.module('yogaClassControllers', []);
 
-yogaClassControllers.controller('YogaClassCtrl', function ($scope, $http) {
+yogaClassControllers.controller('YogaClassCtrl', ['$scope', '$http', 
+	function ($scope, $http) {
 	$http.get('classes/classes.json').success(function(data) {
 		$scope.classes = data;
 	});
@@ -10,4 +11,9 @@ yogaClassControllers.controller('YogaClassCtrl', function ($scope, $http) {
 	$scope.dayNumber = 'dayNumber';
 
 	$scope.studios = {capitolHill: false, wedgewood: false};
-});
+}]);
+
+yogaClassControllers.controller('YogaClassDetailCtrl', ['$scope', '$routeParams',
+	function($scope, $routeParams) {
+			$scope.classID = $routeParams.classID;
+}]);
