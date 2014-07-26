b56a4c1dba56a8f264ecbc83bb80b0c46f049581
MeredithU <me@meredithunderell.com>
2014-07-25 19:22:59 -0700

    Created a RESTful Node API for the yoga class data

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -36,19 +36,19 @@ yogaClassControllers.controller('LocationMapCtrl', ['$scope',
 
 yogaClassControllers.controller('YogaClassCtrl', ['$scope', '$http', 
 	function($scope, $http) {
-	$http.get('classes/classes.json').success(function(data) {
-		$scope.classes = data;
-	});
+		$http.get('/api/yogaclasses').success(function(data) {
+			$scope.classes = data;
+		});
 
-	$scope.dayNumber = 'dayNumber';
+		$scope.dayNumber = 'dayNumber';
 
-	$scope.classNames = {allLevelsFlowClass: false, levelOneClass: false, levelTwoClass: false}
+		$scope.classNames = {allLevelsFlowClass: false, levelOneClass: false, levelTwoClass: false}
 
-	$scope.studios = {capitolHill: false, wedgewood: false};
+		$scope.studios = {capitolHill: false, wedgewood: false};
 
-	$scope.levels = {allLevels: false, levelOne: false, levelTwo: false};
-
-}]);
+		$scope.levels = {allLevels: false, levelOne: false, levelTwo: false};
+	}
+]);
 
 /*yogaClassControllers.controller('YogaClassDetailCtrl', ['$scope', '$routeParams',
  	function($scope, $routeParams) {
--- server.js
+++ server.js
@@ -5,7 +5,16 @@ var http = require('http');
  
 var app = express();
 app.use(express.static( __dirname + '/app'));
-app.set('port', process.env.PORT || 3000);
+app.set('port', process.env.PORT || 8000);
+
+// Node API =================================
+
+app.get('/api/yogaclasses', function(req, res) {
+	var yogaClasses = require(__dirname + '/app/classes/classes.json');
+	res.json(yogaClasses);
+});
+
+// Listen   =================================
  
 var server = http.createServer(app);
 server.listen(app.get('port'), function() {
